b88eec3b09d658b1d2fe4264d8c653215047b805
Hassan Esmaeili <hassan.es975@gmail.com>
2017-07-20 15:22:05 +0700

    Add finetune inception_v4 on leafs script

---
+++ scripts/finetune_inception_v4_on_leafs.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+#
+# This script performs the following operations:
+# 1. Downloads the leafs dataset
+# 2. Fine-tunes an InceptionV3 model on the leafs training set.
+# 3. Evaluates the model on the leafs validation set.
+#
+# Usage:
+# cd slim
+# ./slim/scripts/finetune_inception_v4_on_leafs.sh
+set -e
+
+# Where the pre-trained InceptionV3 checkpoint is saved to.
+PRETRAINED_CHECKPOINT_DIR=/tmp/checkpoints
+
+# Where the training (fine-tuned) checkpoint and logs will be saved to.
+TRAIN_DIR=/tmp/leafs-models/inception_v4
+
+# Where the dataset is saved to.
+DATASET_DIR=/tmp/leafs
+
+# Download the pre-trained checkpoint.
+if [ ! -d "$PRETRAINED_CHECKPOINT_DIR" ]; then
+  mkdir ${PRETRAINED_CHECKPOINT_DIR}
+fi
+if [ ! -f ${PRETRAINED_CHECKPOINT_DIR}/inception_v4.ckpt ]; then
+  wget http://download.tensorflow.org/models/inception_v4_2016_09_09.tar.gz
+  tar -xvf inception_v4_2016_09_09.tar.gz
+  mv inception_v4.ckpt ${PRETRAINED_CHECKPOINT_DIR}/inception_v4.ckpt
+  rm inception_v4_2016_09_09.tar.gz
+fi
+
+# Download the dataset
+python download_and_convert_data.py \
+  --dataset_name=leafs \
+  --dataset_dir=${DATASET_DIR}
+
+# Fine-tune only the new layers for 4000 steps.
+python train_image_classifier.py \
+  --train_dir=${TRAIN_DIR} \
+  --dataset_name=leafs \
+  --dataset_split_name=train \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v4 \
+  --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v4.ckpt \
+  --checkpoint_exclude_scopes=InceptionV4/Logits,InceptionV4/AuxLogits \
+  --trainable_scopes=InceptionV4/Logits,InceptionV4/AuxLogits \
+  --max_number_of_steps=4000 \
+  --batch_size=32 \
+  --learning_rate=0.01 \
+  --learning_rate_decay_type=fixed \
+  --save_interval_secs=60 \
+  --save_summaries_secs=60 \
+  --log_every_n_steps=100 \
+  --optimizer=rmsprop \
+  --weight_decay=0.00004
+
+# Run evaluation.
+python eval_image_classifier.py \
+  --checkpoint_path=${TRAIN_DIR} \
+  --eval_dir=${TRAIN_DIR} \
+  --dataset_name=leafs \
+  --dataset_split_name=validation \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v4
+
+# Fine-tune all the new layers for 2000 steps.
+python train_image_classifier.py \
+  --train_dir=${TRAIN_DIR}/all \
+  --dataset_name=leafs \
+  --dataset_split_name=train \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v4 \
+  --checkpoint_path=${TRAIN_DIR} \
+  --max_number_of_steps=2000 \
+  --batch_size=32 \
+  --learning_rate=0.0001 \
+  --learning_rate_decay_type=fixed \
+  --save_interval_secs=60 \
+  --save_summaries_secs=60 \
+  --log_every_n_steps=10 \
+  --optimizer=rmsprop \
+  --weight_decay=0.00004
+
+# Run evaluation.
+python eval_image_classifier.py \
+  --checkpoint_path=${TRAIN_DIR}/all \
+  --eval_dir=${TRAIN_DIR}/all \
+  --dataset_name=leafs \
+  --dataset_split_name=validation \
+  --dataset_dir=${DATASET_DIR} \
+  --model_name=inception_v4
