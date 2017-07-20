8472499d705c7a85107a2906fff67ecee6e4ed70
Hassan Esmaeili <hassan.es975@gmail.com>
2017-07-20 15:14:21 +0700

    Change finetune inception_v3 step to 4000 and 1000

--- scripts/finetune_inception_v3_on_leafs.sh
+++ scripts/finetune_inception_v3_on_leafs.sh
@@ -35,7 +35,7 @@ python download_and_convert_data.py \
   --dataset_name=leafs \
   --dataset_dir=${DATASET_DIR}
 
-# Fine-tune only the new layers for 1000 steps.
+# Fine-tune only the new layers for 4000 steps.
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR} \
   --dataset_name=leafs \
@@ -45,7 +45,7 @@ python train_image_classifier.py \
   --checkpoint_path=${PRETRAINED_CHECKPOINT_DIR}/inception_v3.ckpt \
   --checkpoint_exclude_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
   --trainable_scopes=InceptionV3/Logits,InceptionV3/AuxLogits \
-  --max_number_of_steps=1000 \
+  --max_number_of_steps=4000 \
   --batch_size=32 \
   --learning_rate=0.01 \
   --learning_rate_decay_type=fixed \
@@ -64,7 +64,7 @@ python eval_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=inception_v3
 
-# Fine-tune all the new layers for 500 steps.
+# Fine-tune all the new layers for 2000 steps.
 python train_image_classifier.py \
   --train_dir=${TRAIN_DIR}/all \
   --dataset_name=leafs \
@@ -72,7 +72,7 @@ python train_image_classifier.py \
   --dataset_dir=${DATASET_DIR} \
   --model_name=inception_v3 \
   --checkpoint_path=${TRAIN_DIR} \
-  --max_number_of_steps=500 \
+  --max_number_of_steps=2000 \
   --batch_size=32 \
   --learning_rate=0.0001 \
   --learning_rate_decay_type=fixed \
